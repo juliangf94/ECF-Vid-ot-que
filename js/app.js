54a8be588cf5c5eaff1b97a965129ecab41bff94
Julian GONZALEZ <juliangf94@hotmail.com>
2023-11-06 15:04:38 +0100

    continue

--- js/app.js
+++ js/app.js
@@ -252,3 +252,12 @@ function addMovie(event) {
 };    
 
 
+// Pass in the id of an element
+let confetti = new Confetti('addBtn');
+
+// Edit given parameters
+confetti.setCount(75);
+confetti.setSize(1);
+confetti.setPower(25);
+confetti.setFade(false);
+confetti.destroyTarget(true);
